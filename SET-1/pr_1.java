1379bc3482f9ab0d94ed1f81a798c1ab8f928381
vadkarvinay <vadkardinesh145@gmail.com>
2026-04-24 14:43:55 +0530

    Create pr_1.java

---
+++ SET-1/pr_1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class practical_1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter distance in meters: ");
+        double meters = sc.nextDouble();
+
+        double feet = meters * 3.28084;
+
+        System.out.printf("Distance in feet = %.2f", feet);
+
+        sc.close();
+    }
+}
